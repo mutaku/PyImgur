b7ac4312d502ad529fa8ea6f75212bae4bbdc2a1
Matthew Martz <xiao_haozi@mutaku.com>
2013-05-30 00:07:35 -0400

    added clipboard support - we write the url to clipboard with xsel (linux)

--- thunar_add.py
+++ thunar_add.py
@@ -3,6 +3,7 @@
 import sys
 import webbrowser
 from pyimgur import UploadImage as U
+from subprocess import Popen, PIPE
 
 try:
     fn = sys.argv[1]
@@ -10,6 +11,8 @@ try:
     if not result.error:
         url = result.imageURL['url']
         webbrowser.open(url)
+	clip = Popen(['xsel', '-ib'], stdin=PIPE)
+	clip.communicate(input=url)
     else:
         print result.error
 except:
